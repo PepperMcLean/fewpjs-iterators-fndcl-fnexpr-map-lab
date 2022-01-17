37447675fc5309e67e439dabf4e88832c5dcec10
PepperMcLean <jackmcl206@gmail.com>
2022-01-16 23:01:50 -0600

    commit tree d323594ccccb3e079a4d4fd18c0e1266bad20203

--- index.js
+++ index.js
@@ -13,5 +13,10 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(" ");
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const newTutorial = capitalizedWords.join(' ')
+    return newTutorial 
+  });
 }
